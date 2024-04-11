8bea76037e2838c67c67c6bea1362b4011241286
Thanh <ttran8800@gmail.com>
2024-04-11 09:19:26 -0500

    spring boot starter

---
+++ cogent-tweet-app/src/main/java/com/tweet/cogent/tweet/app/CogentTweetAppApplication.java
@@ -0,0 +1,13 @@
+package com.tweet.cogent.tweet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogentTweetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogentTweetAppApplication.class, args);
+	}
+
+}
