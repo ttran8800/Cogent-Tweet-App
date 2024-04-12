b9739e8182ee2338b8aa60063d5fa6d43bb612a8
Trung <doant.anthony@gmail.com>
2024-04-12 12:55:42 -0400

    test

--- cogent-tweet-app/src/main/java/com/tweet/cogent/tweet/app/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tweet.cogent.tweet.app.repository;
-
-import com.tweet.cogent.tweet.app.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
--- name/src/app/app.module.ts
+++ name/src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginPageComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
