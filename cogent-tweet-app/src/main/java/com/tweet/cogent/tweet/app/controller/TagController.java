802b29a20bfba3f5136c14a9dd9f43a33ce00920
Thanh <ttran8800@gmail.com>
2024-04-12 09:30:01 -0500

    angular base

---
+++ cogent-tweet-app/src/main/java/com/tweet/cogent/tweet/app/controller/TagController.java
@@ -0,0 +1,12 @@
+package com.tweet.cogent.tweet.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TagController {
+
+
+
+}
